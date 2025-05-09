cc27547ce6397f1636ec49a649df6440856a7564
BBTibbers <youngsuk0311@gmail.com>
2025-05-09 18:29:34 +0900

    0509

---
+++ Assets/02.Script/LoadingScene.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScene : MonoBehaviour
+{
+
+    public int NextSceneIndex = 2;
+
+    public Slider ProgressSlider;
+
+    public TextMeshProUGUI ProgressText;
+
+    private void Start()
+    {
+        StartCoroutine(LoadNextScene_Coroutine());
+    }
+
+    private IEnumerator LoadNextScene_Coroutine()
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(NextSceneIndex);
+        ao.allowSceneActivation = false;
+
+        while (ao.isDone == false)
+        {
+            ProgressSlider.value = ao.progress;
+            ProgressText.text = (ao.progress * 100).ToString("F0") + "%";
+            if (ao.progress >= 0.9f)
+            {
+                ProgressText.text = "Press any key to continue...";
+                if (Input.anyKeyDown)
+                {
+                    ao.allowSceneActivation = true;
+                }
+            }
+            yield return null;
+        }
+    }
+}
--- Assets/02.Script/UI/UI_LoginScene.cs
+++ Assets/02.Script/UI/UI_LoginScene.cs
@@ -3,7 +3,8 @@ using TMPro;
 using UnityEngine;
 using DG.Tweening;
 using System.Security.Cryptography;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class UI_LoginScene : MonoBehaviour
 {
     [Serializable]
@@ -13,7 +14,10 @@ public class UI_LoginScene : MonoBehaviour
         public TMP_InputField PasswordInputField;
         public TMP_InputField PasswordConfirmField;
         public TextMeshProUGUI OutputText;
+        public Button ConfirmButton;
     }
+
+    private const string SALT = "10043420";
 
     public GameObject LoginPanel;
     public GameObject RegisterPanel;
@@ -28,6 +32,7 @@ public class UI_LoginScene : MonoBehaviour
     {
         LoginPanel.SetActive(true);
         RegisterPanel.SetActive(false);
+        LoginCheck();
     }
 
     public void GotoRegister()
@@ -66,7 +71,7 @@ public class UI_LoginScene : MonoBehaviour
         }
 
         // playerPrefs에 저장
-        PlayerPrefs.SetString(id, Encryption(password));
+        PlayerPrefs.SetString(id, Encryption(password+SALT));
         PlayerPrefs.Save();
 
         GotoLogin();
@@ -81,5 +86,48 @@ public class UI_LoginScene : MonoBehaviour
         byte[] hash = sha256.ComputeHash(bytes);
         string hashString = BitConverter.ToString(hash).Replace("-", "").ToLower();
         return hashString;
+    }
+
+    public void Login()
+    {
+        string id = LoginInputField.IDInputField.text;
+        if (string.IsNullOrEmpty(id))
+        {
+            LoginInputField.OutputText.text = "아이디를 입력하세요.";
+            LoginInputField.OutputText.transform.DOShakeScale(0.5f, 0.1f, 10, 90, true);
+            return;
+        }
+        string password = LoginInputField.PasswordInputField.text;
+        if (string.IsNullOrEmpty(password))
+        {
+            LoginInputField.OutputText.text = "비밀번호를 입력하세요.";
+            LoginInputField.OutputText.transform.DOShakeScale(0.5f, 0.1f, 10, 90, true);
+            return;
+        }
+        // playerPrefs에서 가져오기
+        string savedPassword = PlayerPrefs.GetString(id);
+        if (string.IsNullOrEmpty(savedPassword))
+        {
+            LoginInputField.OutputText.text = "아이디와 비밀번호 정보가 일치하지 않습니다.";
+            LoginInputField.OutputText.transform.DOShakeScale(0.5f, 0.1f, 10, 90, true);
+            return;
+        }
+        if (string.Compare(savedPassword, Encryption(password + SALT)) != 0)
+        {
+            LoginInputField.OutputText.text = "아이디와 비밀번호 정보가 일치하지 않습니다.";
+            LoginInputField.OutputText.transform.DOShakeScale(0.5f, 0.1f, 10, 90, true);
+            return;
+        }
+        // 로그인 성공
+        Debug.Log("로그인 성공");
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoginCheck()
+    {
+        string id = LoginInputField.IDInputField.text;
+        string password = LoginInputField.PasswordInputField.text; 
+
+        LoginInputField.ConfirmButton.enabled = ! string.IsNullOrEmpty(id) && !string.IsNullOrEmpty(password);
     }
 }
